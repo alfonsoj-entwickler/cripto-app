b4be3cfc10570c6263020f6d6d0b5d44db0b9bc3
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2025-11-01 11:30:28 +0100

    Add Spinner component for loading indication

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,11 @@
+export default function Spinner() {
+  return (
+    <div className="spinner">
+      <div className="rect1 bg-green-600"></div>
+      <div className="rect2 bg-green-600"></div>
+      <div className="rect3 bg-green-600"></div>
+      <div className="rect4 bg-green-600"></div>
+      <div className="rect5 bg-green-600"></div>
+    </div>
+  );
+}
